9209492996b6eb5b32313e4cf786c44c871990d3
wonjongseo <visionwill@naver.com>
2022-06-08 16:31:15 +0900

    server set up

---
+++ src/init.js
@@ -0,0 +1,9 @@
+import "dotenv/config";
+import app from "./server.js";
+
+const PORT = 4000;
+
+const handleListening = () =>
+    console.log(`✅ Server listening on http://localhost:${PORT}`);
+
+app.listen(PORT, handleListening);
